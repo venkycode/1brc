941d21a34c27b4e2849458213909f72c35c49646
Venkatesh Kotwade <venkateshkotwade@gmail.com>
2024-06-15 23:29:21 +0530

    check output correctness

--- src/main/go/venkycode/run.sh
+++ src/main/go/venkycode/run.sh
@@ -1,15 +1,24 @@
 #! /bin/zsh
-
+set -e
 # Compile the code
 go build -o main
 
+output_file="output.txt"
 # if first arg is "profile"
 if [ $1 = "profile" ]; then
     echo "Profiling the code"
-    time ./main -input '../../../../measurements.txt' -profile
+    time ./main -input '../../../../measurements.txt' -profile > $output_file
 else
     echo "Running the code"
-    time ./main -input '../../../../measurements.txt'
+    time ./main -input '../../../../measurements.txt' > $output_file
 fi
 
 
+correct_output_file="./correct_output.txt"
+
+# Check if the output is correct
+if diff $output_file $correct_output_file; then
+    echo "Output is correct"
+else
+    echo "Output is incorrect"
+fi
